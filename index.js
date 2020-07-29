2b3844a37f7a0168a2eab8117d21ab0e85f67c80
GIFortress <idigits.solutions@gmail.com>
2020-07-29 10:38:50 +0100

    new training set

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var app = new Vue({
     data:{
         message: 'Here is my First Vuejs Apps',
         name: 'GI Fortress Author and Developer',
-        price: '1000',
+        value: 100,
+        price: '2000',
         path: 'imgs/somie.jpg',
         link: 'http://localhost:8080/newdoglord/paypal',
     },
@@ -15,14 +16,14 @@ var app = new Vue({
             return cost;
         }
     }
-})
+});
 
 var app2 = new Vue({
     el: '#app-2',
     data: {
       message: 'You loaded this page on ' + new Date().toLocaleString()
     }
-  })
+  });
 
   var tempapp = new Vue({
     el: '#tempapp',
@@ -38,7 +39,7 @@ var app2 = new Vue({
         },
 
     }
-})
+});
 
 var kbapp = new Vue({
     el: '#kbapp',
@@ -50,7 +51,7 @@ var kbapp = new Vue({
             console.log('key pressed')
         }
     }
-})
+});
 
 var twoway = new Vue({
     el: '#twoway',
@@ -59,7 +60,7 @@ var twoway = new Vue({
         message: '',
         status: false,
     }
-})
+});
 
 var gateapp = new Vue({
     el: '#gateapp',
@@ -67,12 +68,12 @@ var gateapp = new Vue({
     data : {
         gate: false,
     }
-})
+});
 
 Vue.component('todo-item', {
     props: ['todo'],
     template: '<li>{{ todo.name }}, Score: {{ todo.score }}</li>'
-  })
+  });
 
 var loopapp = new Vue({
     el: '#loopapp',
@@ -87,4 +88,92 @@ var loopapp = new Vue({
 
         ]
     }
+});
+
+var vueinstan = new Vue({
+    el: '#vueinstan',
+
+    data : {
+       value: 200,
+        
+    },
+    methods: {
+        f1: function() {
+            return app.price;
+        }
+
+    }
+});
+
+
+Vue.component('mycomponent',{
+    template: '<p>This is my component {{ name }} - temp:{{ temprature }}<sup>0</sup>C <br/><button v-on:click="decTemp()" class="status">decrease Temp</button> - <button v-on:click="changeTemp()" class="status">Increase Temp</button> </p>',
+    data:function(){
+        return{
+            name: 'John',
+            temprature: 100,
+        }
+    },
+    methods: {
+        changeTemp:function(){
+            this.temprature = this.temprature + 5;            
+        },
+        decTemp:function(){
+            this.temprature = this.temprature - 5;            
+        },
+
+    },
+
+})
+
+
+var compapp = new Vue({
+    el: '#compapp',
+
+    data : {
+       value: 200,
+       price: 2500,
+        
+    },
+    methods: {
+        
+
+    }
+});
+
+Vue.component('fontsize',{
+        template: '<p style="{font-size: }">{{ fontsz }} Lorem ipsum dolor <br/><button v-on:click="incFontsz()">Incr.</button> Font Size <button v-on:click="decFontsz()">Decr.</button></p>',
+        data:function(){
+            return{
+                fontsz: 14,
+            }
+        },
+        methods: {
+            incFontsz:function(){
+                this.fontsz = this.fontsz + 2;  
+            },
+            decFontsz:function(){
+                this.fontsz = this.fontsz - 2;  
+            },
+    
+        },
+
+    })
+
+var vref = new Vue({
+    el: '#vref',
+
+    data : {
+        years: 36/12,
+        rate: 12/100,
+        inter: '',
+                        
+    },
+    methods: {
+        interest: function(){
+            this.inter = this.$refs.principal.value * this.years * this.rate;
+            console.log(this.inter);
+        }
+    }
+    
 })
